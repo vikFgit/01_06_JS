26a3ce7ace98f3182c9bcc1a00ccfbfdc3c80bee
viktoriia <viktoriia0Lv09@gmail.com>
2022-06-02 22:55:05 +0300

    task2

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 // написать функцию, которая сравнивает 2 значения одного типа и возвращает большее, если сравнение невозможно, то вернуть null
 
-const userInputNum1 = prompt('Enter first number');
-const userInputNum2 = prompt('Enter second number');
-/**
- * comparison of values of the same type
- * @param {any} value1 
- * @param {any} value2 
- * @returns any | null
- */
-const compareSameValuesType = function(value1, value2){
-  if(typeof value1===typeof value2 && value1>value2){
-    return value1;
-  }else{
-    return value2;
-  }
-}
-if(typeof value1!==typeof value2){
-  console.log(null);
-}
-console.log(compareSameValuesType(userInputNum1, userInputNum2));
+// const userInputNum1 = prompt('Enter first number');
+// const userInputNum2 = prompt('Enter second number');
+// /**
+//  * comparison of values of the same type
+//  * @param {any} value1 
+//  * @param {any} value2 
+//  * @returns any | null
+//  */
+// const compareSameValuesType = function(value1, value2){
+//   if(typeof value1===typeof value2 && value1>value2){
+//     return value1;
+//   }else{
+//     return value2;
+//   }
+// }
+// if(typeof value1!==typeof value2){
+//   console.log(null);
+// }
+// console.log(compareSameValuesType(userInputNum1, userInputNum2));
+
+
+
 
 // variant2
 // const compareSameValuesType = function(value1, value2){
@@ -31,3 +34,16 @@ console.log(compareSameValuesType(userInputNum1, userInputNum2));
 //  return null;
 // }
 // }
+// console.log(compareSameValuesType(5, true));
+
+
+// написать функцию, которая принимает число и выводит true, если число четное, и false, если число нечетное
+
+const isEvenNumber = function(value){
+if(value %2===0){
+  return true;
+}else{
+  return false;
+}
+}
+console.log(isEvenNumber(10));
